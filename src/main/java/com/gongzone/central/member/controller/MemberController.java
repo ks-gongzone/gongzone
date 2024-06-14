90b30210cd6b49692b7c700a4dd7903db1bb2508
Huroad <cabin9999@naver.com>
2024-06-14 14:07:05 +0900

    [GZ-17]feat:로그인 구현

--- src/main/java/com/gongzone/central/member/controller/MemberController.java
+++ src/main/java/com/gongzone/central/member/controller/MemberController.java
@@ -1,9 +1,7 @@
-package com.gongzone.central.member.login.controller;
+package com.gongzone.central.member.controller;
 
 import com.gongzone.central.member.domain.Member;
 import com.gongzone.central.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +21,19 @@ public class MemberController {
     public ResponseEntity<Member> register(@RequestBody Member member) {
         Member registeredMember = memberService.registerMember(member);
         return ResponseEntity.ok(registeredMember);
+    }
+
+    @PostMapping
+    public ResponseEntity<Member> login(@RequestBody Member member) {
+        return null;
+    }
+
+    public ResponseEntity<Member> findMemberById(@RequestBody Member member) {
+        return null;
+    }
+
+    public ResponseEntity<List<Member>> findAllMembers() {
+        return null;
     }
 
 }
