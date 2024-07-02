41aa93e64b5a5b57ecdf29ceb67c41926a90de27
Huroad <cabin9999@naver.com>
2024-07-02 22:11:27 +0900

    [GZ-31]feat:party statusCode 수정 업데이트 삭제

--- src/main/java/com/gongzone/central/party/accept/mapper/AcceptMapper.java
+++ src/main/java/com/gongzone/central/party/accept/mapper/AcceptMapper.java
@@ -2,12 +2,18 @@ package com.gongzone.central.party.accept.mapper;
 
 import com.gongzone.central.party.accept.domain.AcceptDetail;
 import com.gongzone.central.party.accept.domain.AcceptMember;
+import com.gongzone.central.utils.StatusCode;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface AcceptMapper {
-    AcceptDetail getPartyDetail(String partyId);
-    List<AcceptMember> getParticipants(String PartyId);
+    AcceptDetail getPartyDetail(String partyNo);
+    List<AcceptMember> getParticipants(String partyNo);
+
+    String findPointNoByMemberNo(@Param("partyId") String partyId);
+    void updatePartyStatus(String partyId, StatusCode statusCode);
+    void deletePartyStatus(String partyId);
 }
