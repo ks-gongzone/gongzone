3000318810d93c2fcc20ecb0f15989c9c628e3a8
Huroad <cabin9999@naver.com>
2024-07-04 09:48:57 +0900

    [GZ-17]refactory:absorption

---
+++ src/main/java/com/gongzone/central/party/accept/domain/AcceptPartyList.java
@@ -0,0 +1,12 @@
+package com.gongzone.central.party.accept.domain;
+
+public class AcceptPartyList {
+    private String partyNo;
+    private String boardNo;
+    /*private String partyNo;
+    private String partyNo;
+    private String partyNo;
+    private String partyNo;
+    private String partyNo;
+    private String partyNo;*/
+}
