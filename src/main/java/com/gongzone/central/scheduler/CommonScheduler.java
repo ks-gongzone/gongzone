f644466fd0dd047f334d3a741ba21507e8ce4522
danjr0829 <danjr@naver.com>
2024-07-16 13:45:30 +0900

    [GZ-24]✨feat:board status update added

---
+++ src/main/java/com/gongzone/central/scheduler/CommonScheduler.java
@@ -0,0 +1,21 @@
+package com.gongzone.central.scheduler;
+
+import com.gongzone.central.board.mapper.BoardMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class CommonScheduler {
+
+    private final BoardMapper boardMapper;
+
+    @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행
+    public void boardStatusUpdate() {
+        boardMapper.updateBoardPeriodStatus();
+    }
+}
