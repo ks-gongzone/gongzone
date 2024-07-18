aff75d02bde2c1553b576bcee1e79850a28caa88
Huroad <cabin9999@naver.com>
2024-07-18 16:49:07 +0900

    [GZ-17]feat: board wishList

--- src/main/java/com/gongzone/central/board/mapper/BoardMapper.java
+++ src/main/java/com/gongzone/central/board/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.gongzone.central.board.mapper;
 
 import com.gongzone.central.board.domain.Board;
+import com.gongzone.central.board.domain.BoardMyPage;
 import com.gongzone.central.board.domain.BoardReply;
 import com.gongzone.central.board.domain.BoardSearchRequest;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,29 +13,48 @@ public interface BoardMapper {
     void updateBoardPeriodStatus();
 
     void deleteParty(String partyNo);
+
     void deleteBoard(String boardNo);
+
     int countPartyMember(String partyNo);
+
     int getFileNo(String boardNo);
+
     String getPartyNo(String boardNo);
+
     void updatePartyMember(Board board);
+
     void updateParty(Board board);
+
     void updateLocation(Board board);
+
     void updateBoard(Board board);
 
     List<Board> getBoardInfo(String boardNo);
 
     void deleteWish(String boardNo, String memberNo);
+
     void insertWish(String boardNo, String memberNo);
 
     void updateViewCount(String boardNo);
 
     void insertBoard(Board board);
+
     void insertLocation(Board board);
+
     void insertFileRelation(Board board);
+
     void insertParty(Board board);
+
     void insertPartyMember(Board board);
 
     List<Board> getBoardList(BoardSearchRequest request);
+
     List<BoardReply> getBoardReplyList(String boardNo);
+
     int getBoardWish(String memberNo, String boardNo);
+
+    List<BoardMyPage> wishListMyPage(String memberNo);
+
+    String filePathByboardNo(String boardNo);
 }
